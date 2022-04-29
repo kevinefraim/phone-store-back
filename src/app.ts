edcc3ae4132051e2ae94f18ec395d8a787677ab2
Kevin Efraim <kevinefraim02@gmail.com>
2022-04-29 15:23:59 -0300

    admin

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", ""],
+    origin: ["http://localhost:3000", "http://localhost:3001"],
   })
 );
 app.use(express.json());
